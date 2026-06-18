c56e2557fadeb9c56b19257df212a507b15f190e
tamar-bunturi <tamara.bunturi@gmail.com>
2026-06-18 20:36:48 +0400

    Add GitHub Actions CI workflow to run tests on every push

--- src/main/java/io/github/tamar/bunturi/drivers/DriverManager.java
+++ src/main/java/io/github/tamar/bunturi/drivers/DriverManager.java
@@ -2,14 +2,20 @@ package io.github.tamar.bunturi.drivers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
     private WebDriver driver;
 
-    public void startDriver (String browser){
-        if(browser.equalsIgnoreCase("chrome")){
-            driver = new ChromeDriver();
-        } else{
+    public void startDriver(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            // On a CI server there is no screen, so run Chrome headless.
+            if (System.getenv("CI") != null) {
+                options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
+            }
+            driver = new ChromeDriver(options);
+        } else {
             throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
         driver.manage().window().maximize();
